24e9a9d8e92c426c981347907157419ac302ec1a
Diego Cando <diego.cando@hsbcad.com>
2023-08-29 12:26:51 -0500

    test

---
+++ test/ClassTest/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassTest
+{
+    public class Class1
+    {
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public method");
+        }
+        private void PrivateMethod()
+        {
+            Console.WriteLine("Private method");
+        }
+        protected void ProtectedMethod()
+        {
+            Console.WriteLine("Protected method");
+        }
+    }
+}
