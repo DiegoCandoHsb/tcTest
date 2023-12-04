0ec621f9dd5768e6f1b1700919007b325349d9fd
Diego Cando <diego.cando@hsbcad.com>
2023-12-04 16:36:23 -0500

    class test

--- test/ClassTest/Class1.cs
+++ test/ClassTest/Class1.cs
@@ -4,15 +4,41 @@
     {
         public void PublicMethod()
         {
+            PrivateMethod();
+            ProtectedMethod();
             Console.WriteLine("Public method");
+            Class2 class2 = new Class2();   
+            class2.PublicMethod();
         }
         private void PrivateMethod()
         {
+            string privatev = "private";
             Console.WriteLine("Private method");
         }
         protected void ProtectedMethod()
         {
+        string protectedv = "protected";
             Console.WriteLine("Protected method");
         }
     }
+
+    class Class2 {
+        public void PublicMethod()
+        {
+            PrivateMethod();
+            ProtectedMethod();
+            Console.WriteLine("Public method");
+        }
+        private void PrivateMethod()
+        {
+            string privatev = "private";
+            Console.WriteLine("Private method");
+        }
+        protected void ProtectedMethod()
+        {
+            string protectedv = "protected";
+            Console.WriteLine("Protected method");
+        }
+    }
+
 }
